8147ba7f82956a7885a785fd16e3daf40fcbfc18
Gabrielle Fonseca <oliveirafonseca.gab@gmail.com>
2026-01-08 11:16:08 -0300

    refactor(arquivo.js): update default permissions to include execute access

--- js/arquivo.js
+++ js/arquivo.js
@@ -3,7 +3,7 @@ class Arquivo {
   constructor(nome, conteudo = "") {
     this.nome = nome; // Nome do arquivo
     this.conteudo = conteudo; // Conteúdo textual
-    this.permissoes = "rw-"; // Permissões padrão
+    this.permissoes = "rwe"; // Permissões padrão
     this.dono = "user"; // Dono padrão
     this.criadoEm = new Date(); // Data de criação
     this.atualizadoEm = new Date(); // Última modificação
